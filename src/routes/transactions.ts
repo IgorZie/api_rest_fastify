2f6ebb8aca55495a52e2607ab7062e8af13153d7
Igor Zielosko <igorzielosko.iz@gmail.com>
2024-02-06 22:34:40 -0300

    Criando preHandler para as rotas de transaction, que sao middlewares para tratar algo antes da execucao da regra

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,39 +2,62 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get('/', { 
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
 
     return {
       transactions,
     }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { 
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
+    const { sessionId } = request.cookies
+
     const { id } = getTransactionParamsSchema.parse(request.params)
 
-    const transactions = await knex('transactions').where('id', id).first()
+    const transactions = await knex('transactions')
+    .where({
+      session_id: sessionId,
+      id
+    })
+    .first()
 
     return {
       transactions,
     }
   })
 
-  app.get('/summary', async () => {
+  app.get('/summary', { 
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
+    const { sessionId } = request.cookies
+
     const summary = await knex('transactions')
+      .where('session_id', sessionId)
       .sum('amount', { as: 'amount' })
       .first()
 
     return { summary }
   })
 
-  app.post('/', async (request, reply) => {
+  app.post('/', { 
+    preHandler: [checkSessionIdExists]
+  }, async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
